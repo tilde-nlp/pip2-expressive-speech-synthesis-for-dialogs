4f183d0e1ddd9df62bc9ea71597b5f057c19af67
Dāvis Nicmanis <davis.nicmanis@tilde.lv>
2021-07-16 12:31:48 +0300

    Added shebang to the server run.sh script

--- server/run.sh
+++ server/run.sh
@@ -1,2 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH=tacotron2_gst:$PYTHONPATH python server/main.py \
+#!/bin/bash
+
+PYTHONPATH=tacotron2_gst:$PYTHONPATH python server/main.py \
 	-c server/config.yaml
